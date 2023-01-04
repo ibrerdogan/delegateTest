d977a33df9e1bc619533b2fc31cacc965344985c
ibrerdogan <94644110+ibrerdogan@users.noreply.github.com>
2023-01-04 22:10:29 +0300

    Initial Commit

---
+++ delegateTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  delegateTest
+//
+//  Created by İbrahim Erdogan on 4.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
